08f37180b4b554e18efa7024d14d612216001faa
Collier Dimitris <dimitrios.collier@gmail.com>
2024-06-24 13:22:47 +0300

    Added MetaExprGenerator project

---
+++ Expr.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpLox;
+public abstract class Expr
+{
+    public class Binary(Expr left, Token @operator, Expr right)
+    {
+        readonly Expr _left = left;
+        readonly Token _operator = @operator;
+        readonly Expr _right = right;
+    }
+}
+
